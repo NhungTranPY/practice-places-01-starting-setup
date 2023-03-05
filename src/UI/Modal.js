aaf9205ec679ad85aa6480c287781c01a470da34
NhungTranPY <nhungtranpy91@gmail.com>
2023-03-05 17:12:04 +0700

    454. Hiding the Modal

--- src/UI/Modal.js
+++ src/UI/Modal.js
@@ -8,19 +8,26 @@ export class Modal {
     show() {
         if ('content' in document.createElement('template')) {
             const modalElememts = document.importNode(this.modalTemplateEl.content, true)
-            const modalElememt = modalElememts.querySelector('.modal')
-            const backdropElememt = modalElememts.querySelector('.backdrop')
+            this.modalElememt = modalElememts.querySelector('.modal')
+            this.backdropElememt = modalElememts.querySelector('.backdrop')
             const contentElement = document.importNode(this.contentTemplateEl.content, true)
 
-            modalElememt.appendChild(contentElement)
+            this.modalElememt.appendChild(contentElement)
 
-            document.body.insertAdjacentElement('afterbegin', modalElememt)
-            document.body.insertAdjacentElement('afterbegin', backdropElememt)
+            document.body.insertAdjacentElement('afterbegin', this.modalElememt)
+            document.body.insertAdjacentElement('afterbegin', this.backdropElememt)
         } else {
             // fallback code
             alert(this.fallbackText)
         }
     }
 
-    hide() {}
+    hide() {
+        if(this.modalElememt) {
+            document.body.removeChild(this.modalElememt) // this.modalElement.remove()
+            document.body.removeChild(this.backdropElememt)
+            this.modalElememt = null
+            this.backdropElememt = null
+        }
+    }
 }
