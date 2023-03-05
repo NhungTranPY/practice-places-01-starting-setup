b5c44b08d4d00b1c88ca77ae446038d528e1949a
NhungTranPY <nhungtranpy91@gmail.com>
2023-03-05 10:17:11 +0700

    452. Getting the User Location

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -4,14 +4,29 @@ class PlaceFinder {
         const locateUserBtn = document.getElementById('locate-btn')
 
         locateUserBtn.addEventListener('click', this.locateUserHandler)
-        addressForm.addEventListener('submit', )
+        addressForm.addEventListener('submit', this.findAddressHandler())
     }
 
     locateUserHandler() {
-        //
+        if (navigator.geolocation) {
+            alert('Location feature is not available - please use a more modern browser or manually enter an address')
+            return
+        }
+        navigator.geolocation.getCurrentPosition(
+        successResult => {
+            const coordinates = {
+                lat: successResult.coords.latitude,
+                lng: successResult.coords.longitude,
+            }
+            console.log(coordinates);
+        }, error => {
+            alert('Could not locate you manually. Please enter an address manually!')
+        })
     }
 
     findAddressHandler() {
         // 
     }
 }
+
+const placeFinder = new PlaceFinder()
